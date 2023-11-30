ad154baf42bc8774e3994f4aa99d7f924ec650f6
Gaston Saucedo <gastonsaucedo2001@gmail.com>
2023-11-30 00:48:35 -0300

    loader agregado

--- js/idioma.js
+++ js/idioma.js
@@ -5,7 +5,7 @@ check.addEventListener("click", idioma)
 function idioma() {
     let id = check.checked
     if (id == true) {
-        location.href = ".././english-page/inicioIngles.html"
+        location.href = "../inicioIngles.html"
     } else {
         location.href = "../index.html"
     }
